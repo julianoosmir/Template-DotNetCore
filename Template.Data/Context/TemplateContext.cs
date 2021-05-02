446885bc41ac7b74aee00ac7b39bf006320764b3
julianoosmir <julianoosmir@outlook.com>
2021-05-02 15:11:28 -0300

    Salvar

---
+++ Template.Data/Context/TemplateContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Data.Mapping;
+using Template.Domain.Entities;
+
+namespace Template.Data.Context
+{
+    public class TemplateContext: DbContext
+    {
+        public TemplateContext(DbContextOptions<TemplateContext> options): base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
